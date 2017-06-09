85dfd1f2a7a62774c8da49d4d25982877b621b22
Fabio Bonina <fabiobonina@gmail.com>
2017-06-09 07:06:08 -0300

    teste

--- apiphp/apiphp.php
+++ apiphp/apiphp.php
@@ -1,16 +1,19 @@
 <?php
+
+require_once 'config.php';
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-$dns = "mysql:host=localhost;dbname=system_tec";
-$user = "root";
-$pass = "";
+
+$table = 'login';
 
 
 
 try {
-	$con = new PDO($dns, $user, $pass);
+      $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	if(!$con){
+	if(!$conn){
 		echo "Não foi possivel conectar com Banco de Dados!";
 	};
 
@@ -22,8 +25,8 @@ try {
 		$action = $_GET['action'];
 	}
 
-      if($action == 'read'){            
-            $query = $con->prepare("SELECT * FROM login");
+      if($action == 'read'){
+            $query = $conn->prepare("SELECT * FROM $table");
             $query->execute();
             $result = $query->fetch();
             $resultado = array();
@@ -32,36 +35,94 @@ try {
             //echo json_encode($resultado);
 	}
 
-      if($action == 'create'){
+	if($action == 'create'){
 
-            $nome = $_GET['action'];
-            $email = $_GET['action'];
-            $nickuser = $_GET['action'];
-            $senha = $_GET['action'];
-            $nivel_usuario;
-            $ativo;
-            $datacadastro;
-            $datalogin;
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $nickuser = $_POST['user'];
+            $senha = $_POST['senha'];
+            $nivel_usuario = "0";
+		$ativo = "0";
+		$datacadastro = date("Y-m-d H:i:s");
+		$datalogin = date("Y-m-d H:i:s");
 
-            $query = $con->prepare("SELECT * FROM login");
-            $query->execute();
-            $result = $query->fetch();
-            $resultado = array();
-            array_push($resultado, $result);
-            $res['users'] = $resultado;
-            //echo json_encode($resultado);
+		$sql  = "INSERT INTO $table (nome, email, nickuser, senha, nivel, ativo, data_cadastro, data_ultimo_login) ";
+		$sql .= "VALUES (:nome, :email, :nickuser, :senha, :nivel, :ativo, :data_cadastro, :data_ultimo_login)";
+		$stmt = $conn->prepare($sql);
+		$stmt->bindParam(':nome',$nome);
+		$stmt->bindParam(':email',$email);
+		$stmt->bindParam(':nickuser',$nickuser);
+		$stmt->bindParam(':senha',$senha);
+		$stmt->bindParam(':nivel',$nivel_usuario);
+		$stmt->bindParam(':ativo',$ativo);
+		$stmt->bindParam(':data_cadastro',$datacadastro);
+		$stmt->bindParam(':data_ultimo_login',$datalogin);
+		return $stmt->execute();
+
+            if($stmt){
+		      $res['message'] = "Suscesso, Usuario adicionado";
+	      } else{
+                  $res['error'] = true;
+                  $res['message'] = "Error, Usuario não adicionado ";
+            }
+		
 	}
+
+      if($action == 'update'){
+
+            $id = $_POST['id'];
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $nickuser = $_POST['user'];
+            $senha = $_POST['senha'];
+
+            $sql  = "UPDATE $table SET nome = :nome, email = :email, nickuser = :nickuser, senha = :senha, WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':nome',$nome);
+            $stmt->bindParam(':email',$email);
+            $stmt->bindParam(':nickuser',$nickuser);
+            $stmt->bindParam(':senha',$senha);
+            $stmt->bindParam(':id', $id);
+            return $stmt->execute();
+
+            if($stmt){
+		      $res['message'] = "Suscesso, Usuario atualizado";
+	      } else{
+                  $res['error'] = true;
+                  $res['message'] = "Error, Usuario não atualizado";
+            }
+      }
+
+      if($action == 'delete'){
+
+            $id = $_POST['id'];
+
+            $sql  = "DELETE FROM $table WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $id);
+            return $stmt->execute();
+
+            if($stmt){
+		      $res['message'] = "Suscesso, usuario deletado";
+	      } else{
+                  $res['error'] = true;
+                  $res['message'] = "Error, usuario não deletado";
+            }
+      }
+
+      echo json_encode($res);
+	
 
 } catch (Exception $e) {
 	echo "Erro: ". $e->getMessage();
 };
 
 //RECUPERAÇÃO DO FORMULÁRIO
-$data = file_get_contents("php://input");
-$objData = json_decode($data);
+//$data = file_get_contents("php://input");
+//$objData = json_decode($data);
 
-$nickuser = $objData->username;
-$senha_informada = $objData->password;
+//$nickuser = $objData->username;
+//$senha_informada = $objData->password;
 
 //$nickuser = 'fabio.bonina';
 //$senha_informada = '123abc';
